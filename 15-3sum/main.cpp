5426622b6ec0493d447c38924064f4a6e2e88dfd
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-06-18 21:34:11 +0800

    15. 3sum, updated

--- 15-3sum/main.cpp
+++ 15-3sum/main.cpp
@@ -1,54 +1,106 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <utility>
 
 class Solution {
+
+	std::vector<int> _nums;
+	std::vector<std::vector<int>> _answer;
+
 public:
-	vector<vector<int>> threeSum(const vector<int>& nums) {
+	std::vector<std::vector<int>> threeSum(const std::vector<int>& nums) {
 
-		const size_t len{nums.size()};
-		if (len <= 2) { return vector<vector<int>>{}; }
+		if (nums.size() < 3) { return {}; }
+		if (nums.size() == 3) {
+			if (nums.at(0) + nums.at(1) + nums.at(2) == 0) {
+				return {{nums.at(0), nums.at(1), nums.at(2)}};
+			}
+			return {};
+		}
 
-		vector<int> sortedNums(nums);
-		sort(sortedNums.begin(), sortedNums.end());
+		_nums = {};
+		_answer = {};
 
-		vector<vector<int>> triplets;
-		for (size_t ii{0}; ii < len && sortedNums[ii] <= 0; ++ii) {
-			int expectSum{-sortedNums[ii]};
-			size_t jj{ii+1}, kk{len-1};
-			while (jj < kk) {
-				int actualSum{sortedNums[jj]+sortedNums[kk]};
-				if (actualSum == expectSum) {
-					appendToResult(triplets, sortedNums[ii], sortedNums[jj], sortedNums[kk]);
-					++jj;
-					--kk;
-				}
-				else if (actualSum < expectSum) { ++jj; }
-				else { --kk; }
+		std::vector<int> sortedNums{nums};
+		std::sort(sortedNums.begin(), sortedNums.end());
+				
+		// Trim "duplicate" nums
+		// i.e., if a number occurs more than twice, remove the duplicates.
+		//
+		_nums.push_back(sortedNums.at(0));
+		for (size_t ii{1}; ii < sortedNums.size()-1; ++ii) {
+
+			if (sortedNums.at(ii) == sortedNums.at(ii-1) &&
+					sortedNums.at(ii) == sortedNums.at(ii+1) &&
+					sortedNums.at(ii) != 0) {
+				continue;
+			}
+			_nums.push_back(sortedNums.at(ii));
+		}
+		_nums.push_back(sortedNums.at(sortedNums.size()-1));
+
+		// Now, pull me a twoSum recursively...
+		// 
+		_answer = {};
+		for (size_t ii{0}; ii < _nums.size()-2; ++ii) {
+
+			const std::vector<std::pair<int,int>> twoSumPairs{twoSum(-_nums.at(ii), ii+1)};
+			for (const std::pair<int,int>& ts : twoSumPairs) {
+				addToAnswer({_nums.at(ii), ts.first, ts.second});
 			}
 		}
-		return triplets;
-    }
+		return _answer;
+	}
 
 private:
-	void appendToResult(vector<vector<int>>& result, int a, int b, int c) {
-		vector<int> toAdd{a,b,c};
-		const size_t len{result.size()};
-		if (len == 0) { return result.push_back(toAdd); }
 
-		for (size_t ii{len-1}; ii <= len-1; --ii) {
-			if (result[ii][0] != a) { break; }
-			if (result[ii] == toAdd) { return; }
+	// Find twoSums of _nums[leftIndex:] that sum to target.
+	//
+	std::vector<std::pair<int,int>> twoSum(int target, size_t leftIndex) {
+
+		std::vector<std::pair<int,int>> result;
+		size_t left{leftIndex}, right{_nums.size()-1};
+
+		while (left < right) {
+			int sum{_nums.at(left)+_nums.at(right)};
+			if (sum == target) {
+				result.push_back({_nums.at(left), _nums.at(right)});
+				++left;
+				--right;
+				continue;
+			}
+			if (sum < target) {
+				++left;
+			} else {
+				--right;
+			}
 		}
-		result.push_back(toAdd);
+
+		return result;
+	}
+
+	void addToAnswer(const std::vector<int>& triplet) {
+
+		// It is guaranteed that _answer[ii][0] is sorted.
+		//
+		for (size_t ii{_answer.size()-1}; ii <= _answer.size(); --ii) {
+			if (_answer.at(ii).at(0) != triplet.at(0)) { break; }
+			if (_answer.at(ii) == triplet) { return; }
+		}
+		_answer.push_back(triplet);
 	}
 };
 
 int main() {
-	const vector<int> input{-1,0,1,2,-1,-4};
+
+	const std::vector<int> nums{1,2,-2,-1};
 	Solution soln;
-	vector<vector<int>> output{soln.threeSum(input)};
-	for (const auto& triplet : output) {
-		for (const int& vv : triplet) cout << vv << " ";
-		cout << endl;
+	const std::vector<std::vector<int>> result{soln.threeSum(nums)};
+
+	for (const std::vector<int>& triplet : result) {
+
+		for (const int term : triplet) { std::cout << term << " "; }
+		std::cout << std::endl;
 	}
 }
