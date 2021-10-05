42b7cb58461db96547c48f290fd986b81def56b7
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-10-05 18:50:32 +0800

    70. climbing stairs

--- 70-climbing-stairs/main.cpp
+++ 70-climbing-stairs/main.cpp
@@ -1,12 +1,17 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution {
+class Solution
+{
 public:
-	int climbStairs(int n) {
-		const double sqrt5{sqrt(5)};
-		const double gr{(1.0+sqrt5)/2.0};
-		const double grc{(1.0-sqrt5)/2.0};
-		return int(round(pow(gr,n+1)/sqrt5 - pow(grc,n+1)/sqrt5));
+	int climbStairs(int n)
+	{
+		if (n <= 2) {
+			return n;
+		}
+		int a = 1, b = 2;
+		for (int ii = 2; ii < n; ++ii) {
+			const int c = a + b;
+			a = b;
+			b = c;
+		}
+		return b;
 	}
 };
