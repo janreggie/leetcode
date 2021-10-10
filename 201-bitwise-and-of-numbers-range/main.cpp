9c1655e820068f9b3b2ed5701a02ed65c3817b62
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-10-10 16:12:30 +0800

    201. bitwise and of numbers range

---
+++ 201-bitwise-and-of-numbers-range/main.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+public:
+	int rangeBitwiseAnd(int left, int right)
+	{
+		if (left == right) {
+			return left;
+		}
+
+		return rangeBitwiseAnd(left >> 1, right >> 1) << 1;
+	}
+};
